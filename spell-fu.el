3a19836c693686d5f67573f75e158ec4a4a9497f
Campbell Barton <ideasman42@gmail.com>
2022-02-11 16:48:07 +1100

    Cleanup: use brief SPDX license

--- spell-fu.el
+++ spell-fu.el
@@ -1,5 +1,6 @@
 ;;; spell-fu.el --- Fast & light spelling highlighter -*- lexical-binding: t -*-
 
+;; SPDX-License-Identifier: GPL-2.0-or-later
 ;; Copyright (C) 2020  Campbell Barton
 
 ;; Author: Campbell Barton <ideasman42@gmail.com>
@@ -8,19 +9,6 @@
 ;; Keywords: convenience
 ;; Version: 0.3
 ;; Package-Requires: ((emacs "26.2"))
-
-;; This program is free software; you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License, or
-;; (at your option) any later version.
-
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
-
-;; You should have received a copy of the GNU General Public License
-;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
